a53ba39abc03c9bcccc84591df26e1a3981e017e
choisungwook <kgg1959@naver.com>
2020-10-21 20:58:58 +0900

    Add Websocket 설정

---
+++ src/main/java/ssh2/springboot_ssh_client/websocket/ConstantPool.java
@@ -0,0 +1,7 @@
+package ssh2.springboot_ssh_client.websocket;
+
+public class ConstantPool {
+    public static final String USER_UUID_KEY = "user_uuid";
+    public static final String WEB_SSH_OPERATE_CONNECT = "connect";
+    public static final String WEB_SSH_OPERATE_COMMAND = "command";
+}
---
+++ src/main/java/ssh2/springboot_ssh_client/websocket/WebsockerInterceptor.java
@@ -0,0 +1,38 @@
+package ssh2.springboot_ssh_client.websocket;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+import java.util.UUID;
+
+
+public class WebsockerInterceptor implements HandshakeInterceptor {
+    /***
+     * 핸드쉐이크 전, 세션을 구분할 고유 UUID를 설정
+     * @param request
+     * @param response
+     * @param wsHandler
+     * @param attributes
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        if(request instanceof ServletServerHttpRequest){
+            ServletServerHttpRequest httpRequest =(ServletServerHttpRequest)request;
+
+            String uuid = UUID.randomUUID().toString().replace("-","");
+            attributes.put(ConstantPool.USER_UUID_KEY, uuid);
+        }
+        return false;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+    }
+}
