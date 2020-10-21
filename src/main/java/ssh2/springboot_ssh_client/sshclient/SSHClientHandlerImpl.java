0b445e9007d41d85acaf709cc648bd1947ef922c
choisungwook <kgg1959@naver.com>
2020-10-21 21:13:50 +0900

    Change 웹소켓이 연결되었을 때 ssh상태초기화 클래스

--- src/main/java/ssh2/springboot_ssh_client/sshclient/SSHClientHandlerImpl.java
+++ src/main/java/ssh2/springboot_ssh_client/sshclient/SSHClientHandlerImpl.java
@@ -1,13 +1,28 @@
 package ssh2.springboot_ssh_client.sshclient;
 
+import com.jcraft.jsch.JSch;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.socket.WebSocketSession;
+import ssh2.springboot_ssh_client.websocket.ConstantPool;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+@Slf4j
 public class SSHClientHandlerImpl implements SSHClientHandler{
+    private static Map<String, Object> sshMap = new ConcurrentHashMap<>();
+
     @Override
     public void initConnection(WebSocketSession session) {
-
+        JSch jSch = new JSch();
+        SSHConnectionInfo sshConnectionInfo = new SSHConnectionInfo();
+        sshConnectionInfo.setJSch(jSch);
+        sshConnectionInfo.setSession(session);
+        
+        // 웹소켓 세션과 ssh정보 저장
+        String uuid = String.valueOf(session.getAttributes().get(ConstantPool.USER_UUID_KEY));
+        sshMap.put(uuid, sshConnectionInfo);
     }
 
     @Override
