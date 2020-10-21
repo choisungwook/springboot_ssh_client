e09af0a51934fd1418f0f2bd91175e9fccb5a913
choisungwook <kgg1959@naver.com>
2020-10-21 21:03:05 +0900

    Change uuid주석

--- src/main/java/ssh2/springboot_ssh_client/websocket/WebsockerInterceptor.java
+++ src/main/java/ssh2/springboot_ssh_client/websocket/WebsockerInterceptor.java
@@ -1,5 +1,6 @@
 package ssh2.springboot_ssh_client.websocket;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
@@ -9,7 +10,7 @@ import org.springframework.web.socket.server.HandshakeInterceptor;
 import java.util.Map;
 import java.util.UUID;
 
-
+@Slf4j
 public class WebsockerInterceptor implements HandshakeInterceptor {
     /***
      * 핸드쉐이크 전, 세션을 구분할 고유 UUID를 설정
@@ -26,6 +27,7 @@ public class WebsockerInterceptor implements HandshakeInterceptor {
             ServletServerHttpRequest httpRequest =(ServletServerHttpRequest)request;
 
             String uuid = UUID.randomUUID().toString().replace("-","");
+            log.info("[*] uuid: " + uuid);
             attributes.put(ConstantPool.USER_UUID_KEY, uuid);
         }
         return false;
