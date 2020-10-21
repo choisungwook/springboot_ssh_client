04e7d110bf07386881d84f9b9565d327d6cda359
choisungwook <kgg1959@naver.com>
2020-10-21 20:58:46 +0900

    프로젝트 생성

---
+++ src/main/java/ssh2/springboot_ssh_client/SpringbootSshClientApplication.java
@@ -0,0 +1,13 @@
+package ssh2.springboot_ssh_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSshClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSshClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssh2/springboot_ssh_client/SpringbootSshClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ssh2.springboot_ssh_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSshClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
