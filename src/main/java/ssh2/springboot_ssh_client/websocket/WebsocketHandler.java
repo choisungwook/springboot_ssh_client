3ef57218e575d36f8c6ac1f14041af3829e13360
choisungwook <kgg1959@naver.com>
2020-10-21 21:15:17 +0900

    Change 웹소켓연결 후 ssh연결 상태 초기화

--- src/main/java/ssh2/springboot_ssh_client/websocket/WebsocketHandler.java
+++ src/main/java/ssh2/springboot_ssh_client/websocket/WebsocketHandler.java
@@ -1,16 +1,25 @@
 package ssh2.springboot_ssh_client.websocket;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
+import ssh2.springboot_ssh_client.sshclient.SSHClientHandler;
+import ssh2.springboot_ssh_client.sshclient.SSHClientHandlerImpl;
 
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class WebsocketHandler implements WebSocketHandler {
+    private final SSHClientHandlerImpl sshClientHandler;
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-
+        log.info("[*] WebSocket is connected");
+        sshClientHandler.initConnection(session);
     }
 
     @Override
